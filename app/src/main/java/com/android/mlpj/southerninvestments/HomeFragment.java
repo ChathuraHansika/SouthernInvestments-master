b3a588f013e0b15addcf530d82c23e3e6dbc6bde
MLPJ <mlpjayasekera@gmail.com>
2018-08-23 19:07:34 +0530

    fixes to profile

--- app/src/main/java/com/android/mlpj/southerninvestments/HomeFragment.java
+++ app/src/main/java/com/android/mlpj/southerninvestments/HomeFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 /**
@@ -13,17 +16,38 @@ import android.view.ViewGroup;
  */
 public class HomeFragment extends Fragment {
 
+    private UserLocalStore userLocalStore;
 
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
+    private CircleImageView profImage;
+    private TextView tvName, tvRoute,tvNic, tvTelephone, tvEmail, tvAddress;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        profImage = view.findViewById(R.id.profile_image);
+        tvName = view.findViewById(R.id.tv_sales_rep_name);
+        tvRoute = view.findViewById(R.id.tv_route);
+        tvNic = view.findViewById(R.id.tv_nic);
+        tvTelephone = view.findViewById(R.id.tv_telephone);
+        tvEmail = view.findViewById(R.id.tv_email);
+        tvAddress = view.findViewById(R.id.tv_address);
+
+        userLocalStore = new UserLocalStore(getContext());
+        SalesRep currUser = userLocalStore.getUserDetails();
+
+        //set prof pic here
+
+        tvName.setText(currUser.getName());
+        //tvRoute.setText();
+        tvNic.setText(currUser.getNicNo());
+        //tvTelephone.setText(currUser);
+        tvEmail.setText(currUser.getEmail());
+        tvAddress.setText(currUser.getAddLine1() + ", " + currUser.getAddLine2());
+
+        return view;
     }
 
 }
